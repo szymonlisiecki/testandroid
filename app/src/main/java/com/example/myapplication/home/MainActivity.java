395dde51e61d9fab2f6b8e6ae8f0ce137347cc34
AreTour <a.pieszak@o2.pl>
2019-03-31 22:25:22 +0200

    Dodany górny pasek ikon

--- app/src/main/java/com/example/myapplication/home/MainActivity.java
+++ app/src/main/java/com/example/myapplication/home/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.myapplication.home;
 
 import android.content.Context;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TableLayout;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
@@ -26,6 +29,23 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: starting");
         setupBottonNavigationView();
         Toast.makeText(this, "MainActivity", Toast.LENGTH_SHORT).show();
+        setupViewPager();
+    }
+    private void setupViewPager()
+    {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CameraFragment());
+        adapter.addFragment(new HomeFragment());
+        adapter.addFragment(new MessagesFragment());
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_image2vector1);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
+
     }
 
     private void setupBottonNavigationView(){
