52fa88cc63ac8a27b472195e973f0f40423b9cdc
Wojtek <olejnik.wojciech1@gmail.com>
2019-05-25 19:31:57 +0200

    added universal image loader

--- app/src/main/java/com/example/myapplication/Profile/EditProfileFragment.java
+++ app/src/main/java/com/example/myapplication/Profile/EditProfileFragment.java
@@ -6,8 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Context;
+import android.util.Log;
+import android.widget.ImageView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.Utils.UniversalImageLoader;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+
 
 /**
  * Created by User on 6/4/2017.
@@ -17,11 +25,29 @@ public class EditProfileFragment extends Fragment {
 
     private static final String TAG = "EditProfileFragment";
 
+    private ImageView mProfilePhoto;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_editprofile, container, false);
+        mProfilePhoto = (ImageView) view.findViewById(R.id.profile_photo);
+
+        initImageLoader();
+
+        setProfileImage();
 
         return view;
+    }
+
+    private void initImageLoader(){
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+    private void setProfileImage(){
+        Log.d(TAG, "setProfileImage: setting profile image.");
+        String imgURL = "www.androidcentral.com/sites/androidcentral.com/files/styles/xlarge/public/article_images/2016/08/ac-lloyd.jpg?itok=bb72IeLf";
+        UniversalImageLoader.setImage(imgURL, mProfilePhoto, null, "https://");
     }
 }
