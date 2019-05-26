1672c9d68ccda8ce6ba2e09500979986b22f4497
AreTour <a.pieszak@o2.pl>
2019-05-26 19:35:04 +0200

    Database almost working

--- app/src/main/java/com/example/myapplication/Share/NextActivity.java
+++ app/src/main/java/com/example/myapplication/Share/NextActivity.java
@@ -7,10 +7,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.example.myapplication.Utils.FirebaseMethods;
 import com.example.myapplication.Utils.UniversalImageLoader;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -29,17 +32,22 @@ public class NextActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference myRef;
-    //private FirebaseMethods mFirebaseMethods;
+    private FirebaseMethods mFirebaseMethods;
+
+    private EditText mCaption;
 
     //vars
     private String mAppend = "file:/";
+    private int imageCount = 0;
+    private String imgUrl;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
-
+        mFirebaseMethods = new FirebaseMethods(NextActivity.this);
         setupFirebaseAuth();
+        mCaption = (EditText) findViewById(R.id.caption) ;
 
         ImageView backArrow = (ImageView) findViewById(R.id.ivBackArrow);
         backArrow.setOnClickListener(new View.OnClickListener() {
@@ -57,6 +65,9 @@ public class NextActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.d(TAG, "onClick: navigating to the final share screen.");
                 //upload the image to firebase
+                Toast.makeText(NextActivity.this, "Attempting to upload new photo", Toast.LENGTH_SHORT).show();
+                String caption = mCaption.getText().toString();
+                mFirebaseMethods.uploadNewPhoto(getString(R.string.new_photo), caption, imageCount, imgUrl);
             }
         });
 
@@ -84,7 +95,7 @@ public class NextActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         myRef = mFirebaseDatabase.getReference();
-
+        Log.d(TAG, "onDataChange: image count: " + imageCount);
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -106,7 +117,8 @@ public class NextActivity extends AppCompatActivity {
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
+                imageCount = mFirebaseMethods.getImageCount(dataSnapshot);
+                Log.d(TAG, "onDataChange: image count: " + imageCount);
 
             }
 
