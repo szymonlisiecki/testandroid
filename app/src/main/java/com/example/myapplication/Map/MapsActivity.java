27550015915a2bed8cbdeeecb4fb9dcc8f7223eb
Szymon Lisiecki <szymonlisiecki97@gmail.com>
2019-06-15 12:30:17 +0200

    back to the normal markers

--- app/src/main/java/com/example/myapplication/Map/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/Map/MapsActivity.java
@@ -3,6 +3,10 @@ package com.example.myapplication.Map;
 import android.content.Context;
 
 import android.graphics.Bitmap;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 
 import android.graphics.BitmapFactory;
@@ -10,6 +14,9 @@ import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
@@ -27,6 +34,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -34,9 +43,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Downloader;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -47,7 +66,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int ACTIVITY_NUM = 4;
     private Context mContext = MapsActivity.this;
 
+    private Context context;
+
     private GoogleMap mMap;
+
+
 
     ImageView imageView;
 
@@ -60,6 +83,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     Bitmap bmap;
 int i=0;
 
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
 
     ArrayList<LatLng> danePozycja = new ArrayList<LatLng>();
     ArrayList<String> daneTytul = new ArrayList<String>();
@@ -90,7 +115,6 @@ int i=0;
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
        // setupGridView();
-
     }
 
     /**
@@ -131,34 +155,16 @@ int i=0;
 
                     position = new LatLng(imgLati.get(i), imgLong.get(i));
 
-                    //jeżeli w load damy url zdjęcia to pokaże się jedno zdjęcie, poneważ onBitmapLoaded
-                    //wykonuje sie tylko raz, przed tym jak z firebasa pobieramy zdjecia.
-                    Picasso.get().load(photos.get(i).getImage_path()).into(new Target() {
-                        @Override
-                        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-
-                            bitmap = scaleDown(bitmap, 300, false);
+                   //  bmap = getBitmapFromURL(photos.get(i).getImage_path());
+                //    bmap = getBitmapFromURL("https://firebasestorage.googleapis.com/v0/b/phototracker-54d8a.appspot.com/o/photos%2Fusers%2FRv5OaYsfbuetjsPBx0eCd51igSo2%2Fphoto31?alt=media&token=5bf86ee0-d199-4080-956e-029e634358c4");
 
 
-                            // loaded bitmap is here (bitmap)
-                            mMap.addMarker(new MarkerOptions()
-                                    .position(position)
-                                    .title(photos.get(i).getCaption())
-                                    .icon(BitmapDescriptorFactory.fromBitmap(bitmap))
-                            );
-
-
-                        }
-
-                        @Override
-                        public void onBitmapFailed(Exception e, Drawable errorDrawable) {
-
-                        }
-
-                        @Override
-                        public void onPrepareLoad(Drawable placeHolderDrawable) {}
-                    });
-
+                    // loaded bitmap is here (bitmap)
+                    mMap.addMarker(new MarkerOptions()
+                            .position(position)
+                            .title(photos.get(i).getCaption())
+                            //.icon(BitmapDescriptorFactory.fromBitmap(bmap))
+                    );
 
 
                 }
@@ -189,6 +195,68 @@ int i=0;
         Bitmap newBitmap = Bitmap.createScaledBitmap(realImage, width,
                 height, filter);
         return newBitmap;
+    }
+
+
+    public static  Bitmap downloadImage(String url) {
+        Bitmap bitmap = null;
+        InputStream stream;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inSampleSize = 1;
+
+        try {
+            stream = getHttpConnection(url);
+            bitmap = BitmapFactory.decodeStream(stream, null, bmOptions);
+
+            if(stream!=null){
+                stream.close();
+            }
+
+
+        }
+        catch (IOException e1) {
+            e1.printStackTrace();
+            System.out.println("downloadImage"+ e1.toString());
+        }
+        return bitmap;
+    }
+
+    public static  InputStream getHttpConnection(String urlString)  throws IOException {
+
+        InputStream stream = null;
+        URL url = new URL(urlString);
+        URLConnection connection = url.openConnection();
+
+        try {
+            HttpURLConnection httpConnection = (HttpURLConnection) connection;
+            httpConnection.setRequestMethod("GET");
+            httpConnection.connect();
+
+            if (httpConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                stream = httpConnection.getInputStream();
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("downloadImage" + ex.toString());
+        }
+        return stream;
+    }
+
+
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
